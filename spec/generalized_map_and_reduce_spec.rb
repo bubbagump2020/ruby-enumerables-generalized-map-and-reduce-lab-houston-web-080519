ecbe98b32662817b0c8155f01917237a78b0f839
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-21 02:30:12 +0000

    Automatically backed up by Learn

--- spec/generalized_map_and_reduce_spec.rb
+++ spec/generalized_map_and_reduce_spec.rb
@@ -11,11 +11,11 @@ describe 'my own map' do
   end
 
   it "returns an array with the original values multiplied by 2" do
-    expect(map([1, 2, 3, -9]){|n| n * 2}).to eq([2, 4, 6, -18])
+    expect(map_double([1, 2, 3, -9]){|n| n * 2}).to eq([2, 4, 6, -18])
   end
 
   it "returns an array with the original values squared" do
-    expect(map([1, 2, 3, -9]){|n| n * n}).to eq([1, 4, 9, 81])
+    expect(map_squared([1, 2, 3, -9]){|n| n * n}).to eq([1, 4, 9, 81])
   end
 end
 
@@ -28,12 +28,12 @@ describe 'my own reduce' do
   it "returns a running total when given a starting point" do
     source_array = [1,2,3]
     starting_point = 100
-    expect(reduce(source_array, starting_point){|memo, n| memo + n}).to eq(106)
+    expect(reduce_total(source_array, starting_point){|memo, n| memo + n}).to eq(106)
   end
 
   it "returns true when all values are truthy" do
     source_array = [1, 2, true, "razmatazz"]
-    expect(reduce(source_array){|memo, n| memo && n}).to be_truthy
+    expect(reduce_all_truthy(source_array){|memo, n| memo && n}).to be_truthy
   end
 
   it "returns false when any value is false" do
