c2c28b1a02a750118fe0e4a0e36cde94aca799b9
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-21 02:26:32 +0000

    Automatically backed up by Learn

--- spec/generalized_map_and_reduce_spec.rb
+++ spec/generalized_map_and_reduce_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'my own map' do
   it "returns an array with all values made negative" do
-    expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
+    expect(map_to_negative([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
   end
 
   it "returns an array with the original values" do
